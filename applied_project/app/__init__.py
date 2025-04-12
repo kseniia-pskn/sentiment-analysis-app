f5ed06b1df36fc4083e49059f293803aba3aefc3
Kseniia Piskun <78762092+kseniia-pskn@users.noreply.github.com>
2025-04-12 23:33:59 +0300

    Add files via upload

--- applied_project/app/__init__.py
+++ applied_project/app/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_login import LoginManager
 from .models import db, User
-from .routes import main as main_blueprint
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+
+    # Load app configuration
     app.config.from_pyfile('config.py')
 
-    # Initialize DB
+    # Enable CORS
+    CORS(app)
+
+    # Initialize the database
     db.init_app(app)
 
-    # Set up Login Manager
+    # Set up login manager
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -27,6 +32,5 @@ def create_app():
     app.register_blueprint(main_blueprint)
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
     app.register_blueprint(api_blueprint, url_prefix='/api')
-    
 
     return app
