325963d1ffb5e7e434639e3d2e43be55bf9ccd4e
Kseniia Piskun <78762092+kseniia-pskn@users.noreply.github.com>
2025-04-13 23:43:43 +0300

    Add files via upload

--- applied_project/app/models.py
+++ applied_project/app/models.py
@@ -13,7 +13,7 @@ class User(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
 
     # Relationships
